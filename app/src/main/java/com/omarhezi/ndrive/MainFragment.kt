60b9f9afd4c1fd06bce696a2a0e5673ea87e9b35
Omar Hezi <omar.hezi@mediamonks.com>
2021-11-05 23:10:00 -0300

    Add: Search to main screen

--- app/src/main/java/com/omarhezi/ndrive/MainFragment.kt
+++ app/src/main/java/com/omarhezi/ndrive/MainFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.omarhezi.ndrive.databinding.FragmentMainBinding
 import com.omarhezi.ndrive.showslist.core.ShowsListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,19 +21,24 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        val binding = DataBindingUtil.inflate<FragmentMainBinding>(inflater, R.layout.fragment_main, container, false)
+        binding.searchBox.setOnEditorActionListener { _, _, _ ->
+            viewModel.getShowsListByQuery(binding.searchBox.text.toString())
+            binding.searchBox.setText("")
+            true
+        }
+        binding.lifecycleOwner = this
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.showsLiveData.observe(viewLifecycleOwner) {
-            
+            Toast.makeText(requireContext(), it[0].summary, Toast.LENGTH_SHORT).show()
         }
-
-        viewModel.getShowsListByQuery("")
     }
 
 }
